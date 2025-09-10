d1207d7f490edecbf8a86a8a21f0ed625b63c171
Duc Le <duc.le@stfc.ac.uk>
2025-09-10 02:11:47 +0100

    Refactor alignment script to not use enums

--- reduction_files/DG_alignment.py
+++ reduction_files/DG_alignment.py
@@ -2,23 +2,14 @@ from mantid.simpleapi import *
 from mantidqt.utils.qt import import_qt
 import numpy as np
 from mantidqt.widgets.instrumentview.api import get_instrumentview
-import time
-from mantid.geometry import (
-    SpaceGroupFactory,
-    CrystalStructure,
-    ReflectionGenerator,
-    ReflectionConditionFilter,
-    SpaceGroup,
-    UnitCell,
-)
+from mantid.geometry import SpaceGroupFactory, CrystalStructure, ReflectionGenerator, ReflectionConditionFilter, SpaceGroup, UnitCell
 from scipy.spatial.transform import Rotation as R
 from enum import Enum
 from scipy.optimize import minimize
 from datetime import datetime
-import sys
+import sys, time
 from pathlib import Path
-sys.path.append(Path(__file__).parent))
-from Class_Definitions import MAPS, MERLIN, LET, reflection_condition, UB_METHOD, Peak_picking_methods
+
 #====================================================#
 #Script for determining UB Matrices for samples on machines at ISIS
 #Users also have options for peak prediction and reorientation
@@ -27,67 +18,58 @@ from Class_Definitions import MAPS, MERLIN, LET, reflection_condition, UB_METHOD
 #====================================================#
 #     Lattice Parameters                             #
 lattice_parameters = {
-    "a": ,
-    "b": ,
-    "c": ,
-    "alpha": ,
-    "beta": ,
-    "gamma":
-
+    "a": 5.4, "b": 5.4, "c": 5.4,
+    "alpha": 90, "beta": 90, "gamma": 90
 }
-
 #==================================================#
 #           Runs + Peaks                              #
-run_numbers = [""] #input as a list eg. [“10001”,”10003”,”10004”]
-
-corresponding_hkls = [] # please write in same order the runs are written in
+run_numbers = [52146, 52146]
+corresponding_hkls = [[2,2,0], [1,1,1]] # please write in same order the runs are written in
 # e.g[[h1,k1,l1],[h2,k2,l2]]
 
-peak_picking_method =  #Peak_picking_methods.{METHOD}
-# Peak_picking_methods.MANUAL or Peak_picking_methods.ROI
+peak_picking_method = 'ROI'  # Either 'MANUAL' or 'ROI'
 
-
-UB_method =  #UB_METHOD.{CHOSEN_METHOD} E.G UB_METHOD.ONE_PEAK Available methods:ONE_PEAK, TWO_PEAKS, THREE_PEAKS, FIVEPLUS_PEAKS
-
+UB_method = 'TWO_PEAKS' # Available methods: ONE_PEAK, TWO_PEAKS, THREE_PEAKS, FIVEPLUS_PEAKS
 #ONE_PEAK = Method for known scattering plane + 1 peak
 #TWO_PEAK = Method for if you are able to index 2 non-collinear peaks
 #THREE_PEAK = 3 Peaks, lattice parameters are not required
-
 #FIVEPLUS_PEAKS = First automatically index peaks + find UB algorithm
 #for method = ONE PEAK, vectors defining the scattering plane are required
 vector_1 = []
 vector_2 = []
 
 #    Peak to Orient to u vector
-Reorient = #Enter True or False
-hkl_of_desired_alignment= [,] # please input as a single peak: [h,k,l]
+Reorient = True # Enter True or False
+hkl_of_desired_alignment= [2,2,0] # please input as a single peak: [h,k,l]
 
-Predict_peaks = #Enter true or false
-min_dspacing = #in A^-1
-max_dspacing = #in A^-1
+Predict_peaks = True # Enter true or false
+min_dspacing = 1.5 # in A^-1
+max_dspacing = 9.0 # in A^-1
 
-unit_cell_space_group = #this either needs to be a string of the symbol e.g "F d -3 m" or and integer with no "",
+unit_cell_space_group = "F d -3 m" # this either needs to be a string of the symbol e.g "F d -3 m" or and integer with no "",
 # if you don't know your space group number enter "N/A"
-unit_cell_symmetry = reflection_condition.PRIMITIVE
+unit_cell_symmetry = 'PRIMITIVE'
 #Use from available list and enter in the format reflection_condition.{CELL_SYMMETRY} list and enter in the format reflection_condition.{CELL_SYMMETRY},
 # PRIMITIVE by default change by replacing PRIMITIVE with desired symmetry condition
+# which are any of: PRIMITIVE, BODY_CENTERED, C_FACE_CENTERED, B_FACE_CENTERED, A_FACE_CENTERED, ALL_FACE_CENTERED, RHOMBO_CENTERED_REV, HEX_CENTERED_REV
 
 #===============================#
 #   Staff Input
-instrument_name =  #Enter instrument name in all caps, example: MAPS
-cycle = '2025_1'
-rbnum = ''
-
+instrument_name = 'INSTRUMENT_NAME'
+cycle = 'CYCLE_ID'
+rbnum = 'USER_RB_FOLDER'
 
 cycle_shortform = cycle[2:] if cycle.startswith('20') else cycle
-datadir = f'/data/instrument/{instrument_name}/CYCLE20{cycle_shortform.replace("_","")}/RB{rbnum}/'
+savedir = f'/data/analysis/{instrument_name}/RBNumber/{rbnum}/'
+datadir = f'/data/instrument/{instrument_name}/CYCLE20{cycle_shortform.replace("_","")}/{rbnum}/'
 
-
-d_tolerance = #Tolerenace on peak position, in d-spacing in A^-1
-hkl_tolerance = #Used in UB quality validation, for how close indexes calculated have to be to whole
+d_tolerance = 0.1 # Tolerenace on peak position, in d-spacing in A^-1
+hkl_tolerance = 0.2 # Used in UB quality validation, for how close indexes calculated have to be to whole
+gonio_axes = {"Axis0":"Rot,0,1,0,1"} # See https://docs.mantidproject.org/nightly/algorithms/SetGoniometer-v1.html
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
                 #Output Details#
+config['default.instrument'] = instrument_name
 
 ##Checking quality of inputs, this checks if there are enough listed hkls for peaks given
 if len(run_numbers) != len(corresponding_hkls):
@@ -96,12 +78,11 @@ if len(run_numbers) != len(corresponding_hkls):
 #this block gets a list of all the space group symbols in mantid, and their corresponding numbers
 spgr_symbols = SpaceGroupFactory.getAllSpaceGroupSymbols()
 spgr_numbers = [SpaceGroupFactory.createSpaceGroup(symbol).getNumber() for symbol in spgr_symbols]
-space_group ={}
+space_group = {}
 
 #dictionary with space group symbol, & corresponding number for dictionary lookup
 for x in range(len(spgr_symbols)):
     space_group[spgr_symbols[x]] = spgr_numbers[x]
-
 
 #working out mantid allowed space group from input
 keys = lattice_parameters.keys()
@@ -133,20 +114,16 @@ for x in range(len(run_numbers)):
 
 #Manual peak picking method
 match peak_picking_method:
-    case Peak_picking_methods.MANUAL:
+    case "MANUAL":
         for x in range(len(run_numbers)):
-            if instrument_name == LET:
-                ws = Load(str(datadir + instrument_name.i_name[0:3] +"000"+ run_numbers[x]), EnableLogging=False)
-            else:
-                ws = Load(str(datadir+instrument_name.i_name[0:3]+run_numbers[x]),  EnableLogging=False)
-
-            ws = ConvertUnits(ws, Target='dSpacing', EMode='Elastic', EFixed=None,  EnableLogging=False) #converting  TOF to dspacing
+            ws = Load(f'{datadir}/{instrument_name[:3]}{run_numbers[x]}', EnableLogging=False)
+            ws = ConvertUnits(ws, Target='dSpacing', EMode='Elastic', EFixed=None, EnableLogging=False) #converting  TOF to dspacing
 
             #if Ei exists delete it as it causes peak pick to error
             if ws.getRun().hasProperty("Ei"):
                 DeleteLog('ws', 'Ei', EnableLogging=False)
 
-            SetGoniometer(ws,**instrument_name.axes,  EnableLogging=False) # ** unravels dictionary containing axes, + sets goniometer on each workspace
+            SetGoniometer(ws, **gonio_axes, EnableLogging=False)
 
             #displays IV for each run
             iv = get_instrumentview(ws)
@@ -172,18 +149,14 @@ match peak_picking_method:
 
 
 ##ROI + pick best pixel
-    case Peak_picking_methods.ROI:
+    case "ROI":
         for x in range(len(run_numbers)):
-            if instrument_name == LET:
-                ws = Load(str(datadir + instrument_name.i_name[0:3] +"000"+ run_numbers[x]),  EnableLogging=False)
-            else:
-                ws = Load(str(datadir+instrument_name.i_name[0:3]+run_numbers[x]),  EnableLogging=False)
-
+            ws = Load(f'{datadir}/{instrument_name[:3]}{run_numbers[x]}', EnableLogging=False)
             #creates peak table on first iteration
             if x == 0:
                 CreatePeaksWorkspace(ws, 0, OutputWorkspace="SingleCrystalPeakTable",  EnableLogging=False)
             ws = ConvertUnits(ws, Target='dSpacing', EMode='Elastic', EFixed=None,  EnableLogging=False)#converting TOF to dspacing
-            SetGoniometer(ws, **instrument_name.axes, EnableLogging=False)
+            SetGoniometer(ws, **gonio_axes, EnableLogging=False)
 
             if ws.getRun().hasProperty("Ei"):
                 DeleteLog('ws', 'Ei', EnableLogging=False)
@@ -229,6 +202,9 @@ match peak_picking_method:
             # deleting for good file management
         SingleCrystalPeakTable = mtd["SingleCrystalPeakTable"]
 
+    case _:
+        raise RuntimeError(f'Unknown peak picking method {peak_picking_method}')
+
 Number_of_peaks = SingleCrystalPeakTable.getNumberPeaks()
 used_hkls = {}
 for x in range(len(corresponding_hkls)):
@@ -238,32 +214,31 @@ for x in range(len(corresponding_hkls)):
         logger.warning(f"The {hkl_provided} peak may not be indexed correctly, the dspacing calculated from hkl on workspace for this peak was {peak_predicted_d}, d spacing expected for this peak is {corresponding_d[x]}")
 
 match UB_method:
-    case UB_METHOD.ONE_PEAK:
+    case "ONE_PEAK":
         if Number_of_peaks > 1:
             logger.information("This method gives a rough estimate, CalculateUMatrix or FindUBUsingLatticeParameters might be more appropriate")
         FindUBFromScatteringPlane(Vector1=vector_1, Vector2=vector_2, **lattice_parameters, PeaksWorkspace="SingleCrystalPeakTable",  EnableLogging=False)
         used_hkls[run_numbers[0]]= np.array(corresponding_hkls[0])
-    case UB_METHOD.TWO_PEAKS:
+    case "TWO_PEAKS":
         if Number_of_peaks < 2:
             raise ValueError("Not Enough Peaks to use Calculate U matrix method")
         else:
             CalculateUMatrix(PeaksWorkspace="SingleCrystalPeakTable", **lattice_parameters,  EnableLogging=False)
         ##checking output UB is reasonable
-    case UB_METHOD.THREE_PEAKS:
+    case "THREE_PEAKS":
         if Number_of_peaks < 3:
             raise ValueError("Not Enough Peaks to use FindUBUsingIndexedPeaks")
         FindUBUsingIndexedPeaks("SingleCrystalPeakTable", tolerance=tolerance,  EnableLogging=False)
 
-    case UB_METHOD.FIVEPLUS_PEAKS:
+    case "FIVEPLUS_PEAKS":
         if Number_of_peaks < 5:
             raise ValueError("Not enough peaks to use FindUBUsingLatticeParameters")
         else:
             FindUBUsingLatticeParameters(PeaksWorkspace="SingleCrystalPeakTable", **lattice_parameters, NumInitial=Number_initial_peaks,  EnableLogging=False)
-
     case _:
-        raise ValueError("Method Entered doesn't exist")
+        raise ValueError(f"UB Method {UB_method} doesn't exist")
 
-if 2<=UB_method.value<= 4:
+if UB_method != 'ONE_PEAK':
     Number_of_peaks = SingleCrystalPeakTable.getNumberPeaks()
     temporary_workspace = CloneWorkspace("SingleCrystalPeakTable", "SingleCrystalPeakTable_temp", EnableLogging=False)
     Indexed=IndexPeaks("temporary_workspace", tolerance=hkl_tolerance, RoundHKLs=1,  EnableLogging=False)
@@ -354,7 +329,7 @@ if Reorient == True:
     v_aligned = v_aligned/v_al_max
 
     #getting phi rotation
-    if len(instrument_name.axes) > 1:
+    if len(gonio_axes) > 1:
         match_1 = re.findall(r"\d", Axes["Axis1"])
         match_1.pop()
         Axis1 = np.array([float(x) for x in match_1])
@@ -368,27 +343,42 @@ if Reorient == True:
                       method="Nelder-Mead")
     Phi = round(np.degrees(result_phi.x[0]),4)
     #writing the file
-    file_loc = datadir + "DG_Alignment_output" + str(ct)
-    data_file = open(file_loc, "x")
-    data_file.write(f"{ct}\n UB Calculation Method: {UB_method}\n\nUsed Peaks: \n{used_peaks}\nUB From Peak Data:\n {np.round(UB_unaligned,4)},\n\n u vector: {np.round(u_unaligned,4)},\n v vector:{np.round(v_unaligned,4)},\n\nTo align q = {hkl_of_desired_alignment} along beam direction:\n\nRotate to Psi0 {theta_user},\n\n Using {Axis1} as second Axis \n\nThe offset from the plane of the provided q is: {Phi} \n\nUB After Psi Rotation:\n {np.round(UB_final,4)},\n\n u vector: {np.round(u_aligned,4)},\n v_vector: {np.round(v_aligned,4)} ")
-
-    #opening and reading the file :)
-    with open(file_loc) as data_file:
-        print(data_file.read())
+    results = f"{ct}\n UB Calculation Method: {UB_method}\n\n" \
+              f"Used Peaks: \n{used_peaks}\n" \
+              f"UB From Peak Data:\n {np.round(UB_unaligned,4)},\n\n" \
+              f" u vector: {np.round(u_unaligned,4)},\n" \
+              f" v vector:{np.round(v_unaligned,4)},\n\n" \
+              f"To align q = {hkl_of_desired_alignment} along beam direction:\n\n" \
+              f"Rotate to Psi0 {theta_user},\n\n" \
+              f" Using {Axis1} as second Axis \n\n" \
+              f"The offset from the plane of the provided q is: {Phi} \n\n" \
+              f"UB After Psi Rotation:\n {np.round(UB_final,4)},\n\n" \
+              f" u vector: {np.round(u_aligned,4)},\n" \
+              f" v_vector: {np.round(v_aligned,4)} "
+    print(results)
+    file_loc = savedir + "DG_Alignment_output" + str(ct)
+    try:
+        data_file = open(file_loc, "x")
+    except FileNotFoundError:
+        print('Could not write output data')
+    else:
+        data_file.write(results)
 
     names = ["SingleCrystalPeakTable", "temporary_workspace"]
     WorkspaceNames = ["Processed Peak Table Unaligned", "Processed Peak Table Aligned"]
 
-
 elif Reorient == False:
     u_unaligned = u_unaligned / u_max
     v_unaligned = v_unaligned / v_max
+    results = f"{ct}\n\n UB Calculation Method: {UB_method}\n\nUsed Peaks: \n{used_peaks}\nUB From Peak Data:\n {UB_unaligned},\n\n u vector: {u_unaligned},\n v vector:{v_unaligned}"
+    print(results)
     file_loc = datadir + "DG_Alignment_output" + str(ct)
-    data_file = open(file_loc, "x")
-    data_file.write(f"{ct}\n\n UB Calculation Method: {UB_method}\n\nUsed Peaks: \n{used_peaks}\nUB From Peak Data:\n {UB_unaligned},\n\n u vector: {u_unaligned},\n v vector:{v_unaligned}")
-
-    with open(file_loc) as data_file:
-         print(data_file.read())
+    try:
+        data_file = open(file_loc, "x")
+    except FileNotFoundError:
+        print('Could not write output data')
+    else:
+        data_file.write(results)
 
     names = ["SingleCrystalPeakTable"]
     WorkspaceNames = ["Processed Peak Table"]
@@ -398,8 +388,8 @@ RenameWorkspaces(names, WorkspaceNames)
 if Predict_peaks == True:
     InstrumentWidget = import_qt("._instrumentview", "mantidqt.widgets.instrumentview", "InstrumentWidget")
     peaks = mtd["PredictedPeaks"]
-    ws = Load(str(datadir+instrument_name.i_name[0:3]+run_numbers[0]), EnableLogging=False)
-    SetGoniometer(ws, **instrument_name.axes, EnableLogging=False)
+    ws = Load(f'{datadir}/{instrument_name[:3]}{run_numbers[x]}', EnableLogging=False)
+    SetGoniometer(ws, **gonio_axes, EnableLogging=False)
     ws = ConvertUnits(ws, Target='dSpacing', EMode='Elastic', EFixed=None,  EnableLogging=False)
     iv = get_instrumentview(ws)
     iv.container.widget.overlay(peaks.name())
